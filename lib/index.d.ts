0cd96a6b4c7d8335d2be3b5bc06d8dd03a464a9d
kzsolt1984 <kzsolt1984@gmail.com>
2023-02-03 10:48:23 +0100

    lib added

---
+++ lib/index.d.ts
@@ -0,0 +1,5 @@
+import { Strategy, AbstractStrategy } from "./strategy";
+import { MultiSamlStrategy } from "./multiSamlStrategy";
+import type { VerifiedCallback, VerifyWithRequest, VerifyWithoutRequest, MultiStrategyConfig } from "./types";
+export * from "@techteamer/node-saml2";
+export { AbstractStrategy, Strategy, MultiSamlStrategy, VerifiedCallback, VerifyWithRequest, VerifyWithoutRequest, MultiStrategyConfig, };
---
+++ lib/types.d.ts
@@ -0,0 +1,32 @@
+import type * as express from "express";
+import * as passport from "passport";
+import { Profile, SamlConfig } from "@techteamer/node-saml2";
+export interface AuthenticateOptions extends passport.AuthenticateOptions {
+    samlFallback?: "login-request" | "logout-request";
+    additionalParams?: Record<string, any>;
+}
+export interface AuthorizeOptions extends AuthenticateOptions {
+    samlFallback?: "login-request" | "logout-request";
+}
+export interface StrategyOptions {
+    name?: string;
+    passReqToCallback?: boolean;
+}
+export type User = Record<string, unknown>;
+export interface RequestWithUser extends express.Request {
+    samlLogoutRequest: Profile;
+    user: User;
+}
+export type VerifiedCallback = (err: Error | null, user?: Record<string, unknown>, info?: Record<string, unknown>) => void;
+export type VerifyWithRequest = (req: express.Request, profile: Profile | null, done: VerifiedCallback) => void;
+export type VerifyWithoutRequest = (profile: Profile | null, done: VerifiedCallback) => void;
+export type StrategyOptionsCallback = (err: Error | null, samlOptions?: SamlConfig) => void;
+interface BaseMultiStrategyConfig {
+    getSamlOptions(req: express.Request, callback: StrategyOptionsCallback): void;
+}
+export type MultiStrategyConfig = Partial<SamlConfig> & StrategyOptions & BaseMultiStrategyConfig;
+export declare class ErrorWithXmlStatus extends Error {
+    readonly xmlStatus: string;
+    constructor(message: string, xmlStatus: string);
+}
+export {};
